a818c071d3362d2e8361b208e2daeb390cbd34ce
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2016-08-28 02:43:22 +0800

    updata install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Downloading belt ..."
 if [[ $platform == 'Linux' ]]; then
-  curl -o /usr/local/bin/belt https://storage.googleapis.com/waitingkuo-belt/belt-linux
+  sudo curl -o /usr/local/bin/belt https://storage.googleapis.com/waitingkuo-belt/belt-linux
 elif [[ $platform == 'Darwin' ]]; then
   curl -o /usr/local/bin/belt https://storage.googleapis.com/waitingkuo-belt/belt-darwin
 fi
