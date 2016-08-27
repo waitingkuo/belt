13f9a6a9f8eb93b810065cbce6b14e9af12bcef0
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2016-08-28 02:47:34 +0800

    add chmod for belt binary

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ fi
 echo "Downloading belt ..."
 if [[ $platform == 'Linux' ]]; then
   sudo curl -o /usr/local/bin/belt https://storage.googleapis.com/waitingkuo-belt/belt-linux
+  sudo chmod +x /usr/local/bin/belt
 elif [[ $platform == 'Darwin' ]]; then
   curl -o /usr/local/bin/belt https://storage.googleapis.com/waitingkuo-belt/belt-darwin
+  chmod +x /usr/local/bin/belt
 fi
